95400f788149775869fe39676c38fe4d49e1b735
Carlos Saport-Tomas <carlos.saport@gmail.com>
2024-11-20 00:27:08 +0100

    funcionamiento ok con archivos de texto

--- src/bin/decode.rs
+++ src/bin/decode.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::Write;
 use image::open;
 
-const MACRO_PIXEL_SIZE: u32 = 10; // Tamaño de los bloques de píxeles (10x10)
+const MACRO_PIXEL_SIZE: u32 = 1; // Tamaño de los bloques de píxeles (10x10)
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
